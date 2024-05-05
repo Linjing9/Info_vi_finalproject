617835112c999646935bfc029e173e6af7180a8c
Linjing9 <83073780+Linjing9@users.noreply.github.com>
2024-05-05 16:15:09 +0800

    Update main_app.js

--- src/pages/main_app.js
+++ src/pages/main_app.js
@@ -150,16 +150,6 @@ function Final() {
           )}
           </div>
           </div>
-
-            <div className="scroll-down-row">
-              <div className="arrow-container animated fadeInDown">
-                <div className="arrow-2">
-                  <i className="fa fa-angle-down"></i>
-                </div>
-                <div className="arrow-1 animated hinge infinite zoomIn"></div>
-                <button className="scroll-button">Scroll Down</button>
-              </div>
-            </div>
           </Col>
         </Row>
       </section>
@@ -179,15 +169,6 @@ function Final() {
               <IndustryTree width={treemap_width} height={treemap_height} />
             </div>
           </Col>
-        </Row>
-        <Row className="scroll-down-row">
-          <div className="arrow-container animated fadeInDown">
-            <div className="arrow-2">
-              <i className="fa fa-angle-down"></i>
-            </div>
-            <div className="arrow-1 animated hinge infinite zoomIn"></div>
-            <button className="scroll-button">Scroll Down</button>
-          </div>
         </Row>
       </section>
 
